2a200fbe06e4b1f829b52d3f3150c9e840f2b4c3
david-lafontant <lafontant.david@gmail.com>
2022-04-20 12:55:17 -0600

    Update Student class

--- src/student.rb
+++ src/student.rb
@@ -3,10 +3,24 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom)
-    super(age, name: 'Unknown', parent_permission: true)
-    @classroom = classroom
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
+    @classroom = nil
   end
+
+  def add_classroom(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
+  end
+
+
+
+
+
+
+
+
+
 
   def play_hooky
     "¯\(ツ)/¯"
