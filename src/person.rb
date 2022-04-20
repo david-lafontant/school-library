6b55264d125be9f4438b1a9f18a542881847e2ae
david-lafontant <lafontant.david@gmail.com>
2022-04-20 09:47:08 -0600

    Fix Person class inheritance from Nameable class

--- src/person.rb
+++ src/person.rb
@@ -5,13 +5,15 @@ class Person < Nameable
   attr_reader :id
 
   def initialize(age, name: 'Unknown', parent_permission: true)
+    super()
     @id = rand(1..10_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def of_age?
+
+   def of_age?
     @age >= 18
   end
 
