f2ca40ce0bb837d6f40aadc53ce6343f70e09267
david-lafontant <lafontant.david@gmail.com>
2022-04-22 06:59:27 -0600

    Add list_options method

--- src/app.rb
+++ src/app.rb
@@ -25,3 +25,16 @@ def options(books, patron, rentals)
     end
   end
 end
+
+def list_options
+  puts 'Welcome to School library App!'
+  puts "
+    Please choose an option by entering a number:
+    1- List all books
+    2- List all patron
+    3- Create a person
+    4- Create a book
+    5- Create a rental
+    6- List all rentals for a given person id
+    7- Exit"
+end
